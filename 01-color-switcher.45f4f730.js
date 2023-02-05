f30062deb7c33c8a4550da3463661915870272fa
TanitaL <111281242+TanitaL@users.noreply.github.com>
2023-02-05 17:09:35 +0000

    Deploying to gh-pages from @ TanitaL/goit-js-hw-09@22e90f7df725403933b4e2341aa0fe586c8b8611 🚀

---
+++ 01-color-switcher.45f4f730.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),a=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.45f4f730.js.map
